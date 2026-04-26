0b3d06859632035031141fd83c0c8a1ca9be5527
Atrium <ruqumchik@proton.me>
2026-04-26 12:22:07 +0300

    Update resources.nim

--- src/resources.nim
+++ src/resources.nim
@@ -3,7 +3,7 @@ const
   ID_BTN_BURN = 1002
   ID_LBL_STATUS = 1003
   ID_LBL_PHASE = 1004
-
+  
 type BurnPhase = enum
   phKillProcesses = "Остановка процессов Edge..."
   phRemoveExe = "Удаление EXE и системных файлов..."
@@ -11,3 +11,10 @@ type BurnPhase = enum
   phCleanRegistry = "Зачистка реестра..."
   phRemoveWebView2 = "Удаление WebView2 (по желанию)..."
   phDone = "Edge уничтожен. Рекомендуется перезагрузка."
+
+const
+  WM_UPDATE_PROGRESS = WM_USER + 100
+  WM_BURN_FINISHED   = WM_USER + 101
+
+var gCancelBurn*: bool = false
+
